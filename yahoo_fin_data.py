df0566ba54b39d8841def4ce8701c9cae14b8552
lee-vincent <vinnie@vinnielee.io>
2024-04-19 04:37:39 -0400

    yahoo todo comments

--- yahoo_fin_data.py
+++ yahoo_fin_data.py
@@ -12,6 +12,8 @@ def get_data(ticker: str, save: bool = False):
     raw_df = stock_info.get_data(ticker=ticker, start_date="2011-01-01", end_date="2023-12-31", index_as_date=False, interval="1d")
     raw_df.rename(columns={"date": "timestamp"}, inplace=True)
     raw_df.drop(columns=["ticker"], inplace=True)
+    # we should really validate that this function actually returned data for each day. if a column is missing data,
+    # then we should interpolate a value for it using something like bfill, ffill and interpolation
     if save:
         raw_df.to_csv(f"raw_{ticker}_data.csv", index=False)
     else:
