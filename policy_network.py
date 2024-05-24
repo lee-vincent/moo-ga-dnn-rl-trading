cb677334cf0c5ce6fc4125213e9eb74e74f74667
lee-vincent <vinnie@vinnielee.io>
2024-05-24 09:23:41 -0400

    add activation function arg

--- policy_network.py
+++ policy_network.py
@@ -3,20 +3,20 @@ from torch import nn
 
 class PolicyNetwork(nn.Module):
     """A simple feedforward neural network for policy approximation."""
-    def __init__(self, dimensions):
+    def __init__(self, activation_function, dimensions):
         super(PolicyNetwork, self).__init__()  # Call the superclass constructor
         self.dims = dimensions  # The dimensions of the network
         self.network = nn.Sequential(
             nn.Linear(self.dims[0], self.dims[1]),
-            nn.ReLU(),
+            nn.ReLU() if activation_function == 'ReLU' else nn.Tanh(),  # Conditional activation function
             nn.Linear(self.dims[1], self.dims[2]),
-            nn.ReLU(),
+            nn.ReLU() if activation_function == 'ReLU' else nn.Tanh(),  # Conditional activation function
             nn.Linear(self.dims[2], self.dims[3]),
-            nn.ReLU(),
+            nn.ReLU() if activation_function == 'ReLU' else nn.Tanh(),  # Conditional activation function
             nn.Linear(self.dims[3], self.dims[4]),
-            nn.ReLU(),
+            nn.ReLU() if activation_function == 'ReLU' else nn.Tanh(),  # Conditional activation function
             nn.Linear(self.dims[4], self.dims[5]),
-            nn.ReLU(),
+            nn.ReLU() if activation_function == 'ReLU' else nn.Tanh(),  # Conditional activation function
             nn.Linear(self.dims[5], self.dims[6]),
             nn.Softmax(dim=-1)
         )
